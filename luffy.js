e87c8787dc4a1c8035a80ede7a0c79d706d1a557
Nicholas Zhuk <nicholaszhuk@gmail.com>
2023-01-06 23:01:07 -0800

    made luffy faster

--- luffy.js
+++ luffy.js
@@ -1,11 +1,11 @@
 class Luffy{
     constructor(game){
         this.game = game
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./luffy.png"), 0, 0, 56, 58, 8, 0.15);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./luffy.png"), 0, 0, 56, 58, 8, 0.08);
         
         this.x = -300;
         this.y = 600;
-        this.speed = 250;
+        this.speed = 500;
     };
 
     update() {
